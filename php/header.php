e0e3a71474263574a384d8635cfa5aefae5c3885
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2023-09-05 09:38:48 +0200

    A

---
+++ php/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Gaming Boothouse</title>
+    <!-- Add your CSS styles here -->
+</head>
+
+<body>
+    <h1>Our Webpage V1<h1>
+            <header>
+                <nav>
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="about.php">About Us</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                        <!-- Add more menu items as needed -->
+                    </ul>
+                </nav>
+            </header>
+</body>
+
+</html>
