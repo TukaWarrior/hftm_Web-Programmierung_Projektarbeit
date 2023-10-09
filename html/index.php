662e63e7f3d432ee521727d4a3011933b91e8431
Andres Soria <andres.soria@hftm.ch>
2023-10-09 17:27:24 +0200

    -

--- html/index.php
+++ html/index.php
@@ -8,7 +8,7 @@
     href="https://fonts.googleapis.com/css2?family=Lato&family=Playfair+Display:wght@700&display=swap">
   <link rel="stylesheet" href="../css/styles.css">
   <link rel="stylesheet" href="../css/index.css">
-  <link rel="stylesheet" href="../css/ComicStyle_headlings.css">
+  <link rel="stylesheet" href="../css/ComicStyle.css">
   <link rel="stylesheet" href="../css/Animations.css">
   <title>Index</title>
 </head>
@@ -20,56 +20,19 @@
 
   </div>
 
+  <!-- Top -->
   <div class="container">
     <h1 class="big background-text" content="ever wondered">Ever wondered</h1>
     <h1 class="big background-text" content="what happens when">what happens when</h1>
     <h1 class="big background-text" content="you break your ancle?">you break your ancle?</h1>
     <h1 class="symbol pulse">⬇</h1>
   </div>
-
-  <div class="blank"></div>
-  <!-- <div class="blank">
-    <h1 class="big">well...</h1>
-  </div> -->
-
-  <!-- <div class="containerSecond title">
-    <div class="title-box">
-      <div class="card">
-        <h2>Yes Super-Hitmen!!1</h2>
-        <p>you need them</p>
-      </div>
-    </div>
+  <!--------------------------------------------------------->
+  <div class="blank">
+    <h3 class="mid">storytime!</h3>
+    <h1 class="bigblank background-textV2" content="kapoooow">kapoooow</h1>
   </div>
-
-  <div class="container second">
-    <div class="item-first">
-      <div class="img img-first"></div>
-      <div class="card">
-        <h3>F*CK YES</h3>
-        <p>Heroes are just fucking awesome and can help you in every situation</p>
-        <a href="#">How can they help me</a>
-      </div>
-    </div>
-
-    <div class="item-second">
-      <div class="img img-second"></div>
-      <div class="card">
-        <h3>SUPER-HITMEN!</h3>
-        <p>Take a look at our super-hitmen! you are guaranteed to find a model to suit your needs FOR SURE!!!1</p>
-        <a href="hitman.html">See hitmen</a>
-      </div>
-    </div>
-
-    <div class="item-third">
-      <div class="img img-third"></div>
-      <div class="card">
-        <h3>MAYBE YOU WILL DIE!!</h3>
-        <p>Just kidding but our Hitmen are way too strong for daily tasks. Anyway... you can rent them and abuse them!
-        </p>
-        <a href="#">terms of policy</a>
-      </div>
-    </div> -->
-
+  <!-- Story -->
   <article class="comic">
     <div class="panel">
       <p class="text bottom-right">just a regular day saving the world</p>
@@ -88,14 +51,37 @@
     </div>
     <div class="panel">
       <p class="speech speechImage6">i cant do daily tasks anymore!</p>
-      <p class="text bottom-right">times are getting hard...</p>
+      <p class="text bottom-right">realizing...</p>
     </div>
 
     <div class="panel">
       <p class="text top-left">later that day</p>
-      <p class="speech speechImage7">this is exactly what i need now</p>
+      <p class="speech speechImage7">this is exactly what i was looking for</p>
     </div>
 
+  </article>
+  <!--------------------------------------------------------->
+  <div class="blank">
+    <h3 class="mid">ehhhh ok </br> but </br> whats the point?</h3>
+  </div>
+  <!-- Reveal -->
+  <article class="comic">
+    <div class="panelV2">
+      <p class="speech speechImageHeadquarter">you need help? we offer help!</br>
+        we are a company that hires a superhero team for any purpose.</br>
+        the story above is only one of many possible cases where someone needs help!</p>
+    </div>
+    <div class="panelV2">
+      <p class="text top-left">our team is ready!</p>
+    </div>
+    <div class="panelV2">
+      <p class="text top-right">learn more</p>
+      <div class="topnav">
+        <a <?php if (basename($_SERVER['PHP_SELF']) == 'heroes.php')
+          echo 'class="active"'; ?>
+          href="heroes.php">Heroes</a>
+      </div>
+    </div>
   </article>
 
   </div>
