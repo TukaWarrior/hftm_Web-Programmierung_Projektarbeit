faf7743a9e902639791cf889ffc3297125364fb7
Andres Soria <andres.soria@hftm.ch>
2023-10-09 23:25:12 +0200

    -

--- html/about.php
+++ html/about.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato&family=Playfair+Display:wght@700&display=swap">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Lato&family=Playfair+Display:wght@700&display=swap">
     <link rel="stylesheet" href="../css/styles.css">
     <link rel="stylesheet" href="../css/about.css">
     <title>Your Page Title</title>
@@ -12,8 +13,8 @@
 
 <body>
 
-  <!-- Top Navigation Bar -->
-  <?php include 'element_topnav.php'; ?>
+    <!-- Top Navigation Bar -->
+    <?php include 'element_topnav.php'; ?>
 
     <!-- Content -->
 
