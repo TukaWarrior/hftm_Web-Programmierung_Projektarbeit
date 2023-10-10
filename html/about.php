cbdf0035716f72a1df40ca58b91ff4a08099fb1e
Andres Soria <andres.soria@hftm.ch>
2023-10-10 22:57:42 +0200

    -

--- html/about.php
+++ html/about.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Lato&family=Playfair+Display:wght@700&display=swap">
     <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/ComicStyle.css">
     <link rel="stylesheet" href="../css/about.css">
     <title>About us</title>
 </head>
@@ -16,16 +17,19 @@
     <?php include 'element_topnav.php'; ?>
     <!-- Content -->
     <div class="container">
+        <h2 class="mid">Who we are</h2>
+    </div>
+    <div class="container">
         <div class="containerGrid">
-            <div class="box box-1" style="--img: url(../ressources/images/avatar_andres03.png);" data-text="Andres"></div>
-            <div class="box box-2" style="--img: url(../ressources/images/avatar_luca02.png);" data-text="Luca"></div>
-            <div class="box box-3" style="--img: url(../ressources/images/avatar_nicola02.png);" data-text="Nicola"></div>
+            <div class="box box-1" style="--img: url(../ressources/images/avatar_andres03.png);" data-text="Andres Soria"></div>
+            <div class="box box-2" style="--img: url(../ressources/images/avatar_luca02.png);" data-text="Luca Bützberger"></div>
+            <div class="box box-3" style="--img: url(../ressources/images/avatar_nicola02.png);" data-text="Nicola Jöhr"></div>
         </div>
     </div>
 
     <div class="blank"></div>
 
-    <div class="container"></div>
+    <div class="container">lalalalalalala</div>
 
     <footer>© 2023 Awesome Company</footer>
 
