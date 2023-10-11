e26258e309695eaced5b9f1c13b6f6b9be9c03f3
Andres Soria <andres.soria@hftm.ch>
2023-10-11 17:01:04 +0200

    -

--- html/about.php
+++ html/about.php
@@ -16,20 +16,31 @@
     <!-- Top Navigation Bar -->
     <?php include 'element_topnav.php'; ?>
     <!-- Content -->
-    <div class="container">
-        <h2 class="mid">Who we are</h2>
+
+    <!-- <div class="container">
+        <h2 class="mid">who we are</h2>
+    </div> -->
+
+    <div class="aboutBlank" style="--bgcolor: #1a8cff">
+        <h2 class="mid">who we are</h2>
     </div>
+
     <div class="container">
         <div class="containerGrid">
-            <div class="box box-1" style="--img: url(../ressources/images/avatar_andres03.png);" data-text="Andres Soria"></div>
-            <div class="box box-2" style="--img: url(../ressources/images/avatar_luca02.png);" data-text="Luca Bützberger"></div>
-            <div class="box box-3" style="--img: url(../ressources/images/avatar_nicola02.png);" data-text="Nicola Jöhr"></div>
+            <div class="box box-1" style="--img: url(../ressources/images/avatar_andres03.png);"
+                data-text="Andres Soria"></div>
+            <div class="box box-2" style="--img: url(../ressources/images/avatar_luca02.png);"
+                data-text="Luca Bützberger"></div>
+            <div class="box box-3" style="--img: url(../ressources/images/avatar_nicola02.png);"
+                data-text="Nicola Jöhr"></div>
         </div>
     </div>
 
-    <div class="blank"></div>
+    <div class="aboutBlank" style="--bgcolor: #ffd000">
+        <h2 class="mid">what we do</h2>
+    </div>
 
-    <div class="container">lalalalalalala</div>
+    <div class="container"></div>
 
     <footer>© 2023 Awesome Company</footer>
 
