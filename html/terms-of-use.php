d5b8e20d5e1ad69ec9f09cdf0afe82a451914383
Andres Soria <andres.soria@hftm.ch>
2023-10-12 23:13:24 +0200

    finished terms of use

--- html/terms-of-use.php
+++ html/terms-of-use.php
@@ -9,8 +9,8 @@
     <link rel="stylesheet" href="../css/styles.css">
     <link rel="stylesheet" href="../css/ComicStyle.css">
     <link rel="stylesheet" href="../css/about.css">
-    <link rel="stylesheet" href="../css/FAQ.css">
     <link rel="stylesheet" href="../css/footer.css">
+    <link rel="stylesheet" href="../css/termsOfUse.css">
     <title>Terms of use</title>
 </head>
 
@@ -23,26 +23,96 @@
         <h2 class="mid">Terms of use</h2>
     </div>
 
+
     <div class="terms">
-        <p>Welcome to the super side of life! Please read our Terms of Use with your best superhero voice and an imaginary cape on.</p>
-        <ul>
-            <li>Superhero Attire</li>
-            <li>Flight Patterns</li>
-            <li>Mind-Reading</li>
-            <li>Sidekicks</li>
-            <li>Supervillain Relations</li>
-            <li>Gadgets</li>
-            <li>Heroic Selfies</li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-        <p>By using our superhero rental services, you agree to these terms. In case of any concerns, please contact our Super-Support-Team for assistance. And remember, with great power comes great responsibility!</p>
+
+        <h3>Superhero Attire</h3>
+        <p>
+            While using our services, you are encouraged (but not required) to wear a cape, mask, or other superhero
+            attire. Spandex, however, is entirely at your own risk.
+        </p>
+        </br>
+
+        <h3>Supervillain Rehabilitation</h3>
+        <p>
+            Our company supports supervillain rehabilitation programs. Please report any urge to turn evil promptly.
+        </p>
+        </br>
+
+        <h3>Capes</h3>
+        <p>
+            Capes are fabulous, but wear them with caution.
+            In-flight tangling is a genuine occupational hazard.
+            Company not responsible for cape-related accidents.
+            By using our superhero services, you agree to abide by these terms and join the ranks of the superpowered
+            and super-humorous.
+        </p>
+        </br>
+
+        <h3>Mind Control Waiver</h3>
+        <p>
+            Please be aware that some superheroes have mind-control abilities,
+            but they are trained not to use them to make you do embarrassing things like the chicken dance.
+        </p>
+        </br>
+
+        <h3>Laundry Duty</h3>
+        <p>
+            If any superhero costume gets dirty during a mission,
+            it's your responsibility to wash, dry, and fold it before their next heroic adventure.
+        </p>
+        </br>
+
+        <h3>Butt-Kicking and Taking Names</h3>
+        <p>
+            Please understand that our superheroes may employ witty one-liners when defeating villains.
+            They've been practicing these for years and can't help themselves.
+        </p>
+        </br>
+
+        <h3>Super-Duper Contractual Obligations</h3>
+        <p>
+            By using our superhero team,
+            you agree to bring your own capes and twirl them dramatically when appropriate.
+        </p>
+        </br>
+
+        <h3>Superpower Sharing Policy</h3>
+        <p>
+            While our superheroes are at your service,
+            they may share their powers with you, but only if they find you sufficiently charming or in dire need.
+            Seduction tactics are strictly forbidden!
+        </p>
+        </br>
+
+        <h3>Refunds & Returns</h3>
+        <p>
+            If you are dissatisfied with our superhero services,
+            we will gladly refund your payment in the form of invisible,
+            intangible coins that can be redeemed in your dreams.
+        </p>
+        </br>
+
+        <h3>Dimensional Restrictions</h3>
+        <p>
+            We do not provide services to alternate dimensions.
+            You're on your own when it comes to interdimensional travel.
+        </p>
+        </br>
+
+        <h3>Emergency Cat Rescues</h3>
+        <p>
+            Superheroes are allowed to interrupt their crime-fighting duties for cat-rescue missions.
+            They must return the cat safely to its owner, no matter the circumstances.
+        </p>
+        </br>
+
+        <p>
+            By using our superhero rental services,
+            you agree to these terms.
+            In case of any concerns, please contact our Super-SupportTeam for assistance.
+            And remember, with great power comes great comedic responsibility!
+        </p>
     </div>
 
     <div class="container"></div>
