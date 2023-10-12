e75cfa4591706995e91ddc682c124e215db224ca
Andres Soria <andres.soria@hftm.ch>
2023-10-12 23:53:35 +0200

    centered navbar

--- html/element_topnav.php
+++ html/element_topnav.php
@@ -7,7 +7,7 @@
         <a <?php if (basename($_SERVER['PHP_SELF']) == 'heroesTest.php') echo 'class="active"'; ?> href="heroesTest.php">meet the team</a>
         <a <?php if (basename($_SERVER['PHP_SELF']) == 'contact.php') echo 'class="active"'; ?> href="contact.php">Contact</a>
         <a <?php if (basename($_SERVER['PHP_SELF']) == 'about.php') echo 'class="active"'; ?> href="about.php">About</a>
-        <a <?php if (basename($_SERVER['PHP_SELF']) == 'account.php') echo 'class="active"'; ?> href="#account" class="split">Account</a>
+        <!-- <a <?php if (basename($_SERVER['PHP_SELF']) == 'account.php') echo 'class="active"'; ?> href="#account" class="split">Account</a> -->
     </div>
 </body>
 
