232148040164ba81f52e9f42b0801631e3026a8c
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2023-10-12 22:25:56 +0200

    Renamed Heroes to hero.css

--- html/hero_alpine-bat.php
+++ html/hero_alpine-bat.php
@@ -8,8 +8,9 @@
   <link rel="stylesheet"
     href="https://fonts.googleapis.com/css2?family=Lato&family=Playfair+Display:wght@700&display=swap">
   <link rel="stylesheet" href="../css/styles.css">
-  <link rel="stylesheet" href="../css/heros.css">
-  <title>Meet your Heroes</title>
+  <link rel="stylesheet" href="../css/hero.css">
+  <link rel="stylesheet" href="../css/footer.css">
+  <title>Hero: Alpine Bat</title>
 
 </head>
 
@@ -17,7 +18,19 @@
 
   <!-- Top Navigation Bar -->
   <?php include 'element_topnav.php'; ?>
-  
+
+
+  <div class="polaroid-animation">
+      <div class="polaroid-frame">
+        <img class="polaroid-image" src="../ressources/images/heroes/hero_alpine-bat.png" alt="">
+        <h1>Master Chief</h1>
+      </div>
+    </div>
+
+
+  <!-- Bottom Footer Bar -->
+  <?php include 'element_footer.php'; ?>
+
 </body>
 
 </html>
