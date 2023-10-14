85b0f3c3af7dbc30900a8dc2efff74325d493a85
Andres Soria <andres.soria@hftm.ch>
2023-10-14 17:08:29 +0200

    -

--- html/heroTeam.php
+++ html/heroTeam.php
@@ -25,85 +25,80 @@
         <h2 class="mid">our team - the best of the best</h2>
     </div>
     <!-- Team Cards -->
-    <div class="portfolio">
-        <!-- Card 1 -->
-        <div class="portfolio-item">
-            <div class="portfolio-bg" style="--img: url(../ressources/images/heroes/hero_doomslayer.webp);"></div>
-            <div class="portfolio-description">
-                <h4>Doom Slayer</h4>
-                <!-- <p>Best for fighting monsters straight out of hell</p> -->
-                <a class="link-button" href="hero_doomslayer.php">view hero</a>
+    <div class="teamContainer">
+        <div class="portfolio">
+            <!-- Card 1 -->
+            <div class="portfolio-item">
+                <div class="portfolio-bg" style="--img: url(../ressources/images/heroes/hero_doomslayer.webp);"></div>
+                <div class="portfolio-description">
+                    <h4>Doom Slayer</h4>
+                    <a class="link-button" href="hero_doomslayer.php">show details</a>
+                </div>
             </div>
-        </div>
 
-        <!-- Card 2 -->
-        <div class="portfolio-item graphic-design">
-            <div class="portfolio-bg" style="--img: url(../ressources/images/heroes/hero_keanu.webp);"></div>
-            <div class="portfolio-description">
-                <h4>Keanu</h4>
-                <!-- <p>The good guy.</p> -->
-                <a class="link-button" href="hero_keanu.php">view hero</a>
+            <!-- Card 2 -->
+            <div class="portfolio-item graphic-design">
+                <div class="portfolio-bg" style="--img: url(../ressources/images/heroes/hero_keanu.webp);"></div>
+                <div class="portfolio-description">
+                    <h4>Keanu</h4>
+                    <!-- <p>The good guy.</p> -->
+                    <a class="link-button" href="hero_keanu.php">show details</a>
+                </div>
             </div>
-        </div>
 
-        <!-- Card 3 -->
-        <div class="portfolio-item graphic-design">
-            <div class="portfolio-bg" style="--img: url(../ressources/images/heroes/hero_alpinebat.webp);"></div>
-            <div class="portfolio-description">
-                <h4>Alpine Bat</h4>
-                <!-- <p>Switzerlands most mysterious hero. And the only one...</p> -->
-                <a class="link-button" href="hero_alpinebat.php">view hero</a>
+            <!-- Card 3 -->
+            <div class="portfolio-item graphic-design">
+                <div class="portfolio-bg" style="--img: url(../ressources/images/heroes/hero_alpinebat.webp);"></div>
+                <div class="portfolio-description">
+                    <h4>Alpine Bat</h4>
+                    <a class="link-button" href="hero_alpinebat.php">show details</a>
+                </div>
             </div>
-        </div>
 
-        <!-- Card 4 -->
-        <div class="portfolio-item graphic-design">
-            <div class="portfolio-bg" style="--img: url(../ressources/images/heroes/hero_stormtrooper.webp);"></div>
-            <div class="portfolio-description">
-                <h4>FN-3420</h4>
-                <!-- <p>Never hits a target.</p> -->
-                <a class="link-button" href="hero_stormtrooper.php">view hero</a>
+            <!-- Card 4 -->
+            <div class="portfolio-item graphic-design">
+                <div class="portfolio-bg" style="--img: url(../ressources/images/heroes/hero_stormtrooper.webp);"></div>
+                <div class="portfolio-description">
+                    <h4>FN-3420</h4>
+                    <a class="link-button" href="hero_stormtrooper.php">show details</a>
+                </div>
             </div>
-        </div>
 
-        <!-- Card 5 -->
-        <div class="portfolio-item graphic-design">
-            <div class="portfolio-bg" style="--img: url(../ressources/images/heroes/hero_batman.webp);"></div>
-            <div class="portfolio-description">
-                <h4>Batman</h4>
-                <!-- <p>Looks dangerous. And he is.</p> -->
-                <a class="link-button h4" href="hero_batman.php">view hero</a>
+            <!-- Card 5 -->
+            <div class="portfolio-item graphic-design">
+                <div class="portfolio-bg" style="--img: url(../ressources/images/heroes/hero_batman.webp);"></div>
+                <div class="portfolio-description">
+                    <h4>Batman</h4>
+                    <a class="link-button h4" href="hero_batman.php">show details</a>
+                </div>
             </div>
-        </div>
 
-        <!-- Card 6 -->
-        <div class="portfolio-item graphic-design">
-            <div class="portfolio-bg" style="--img: url(../ressources/images/heroes/hero_bean.webp);"></div>
-            <div class="portfolio-description">
-                <h4>Bean</h4>
-                <!-- <p>Finds everything or everyone you're looking for.</p> -->
-                <a class="link-button h4" href="hero_bean.php">view hero</a>
+            <!-- Card 6 -->
+            <div class="portfolio-item graphic-design">
+                <div class="portfolio-bg" style="--img: url(../ressources/images/heroes/hero_bean.webp);"></div>
+                <div class="portfolio-description">
+                    <h4>Bean</h4>
+                    <a class="link-button h4" href="hero_bean.php">show details</a>
+                </div>
             </div>
-        </div>
 
-        <!-- Card 7 -->
-        <div class="portfolio-item graphic-design">
-            <div class="portfolio-bg" style="--img: url(../ressources/images/heroes/hero_laracroft.webp);"></div>
-            <div class="portfolio-description">
-                <h4>Lara Croft</h4>
-                <!-- <p>Find yourself in the jungle? She knows what to do.</p> -->
-                <a class="link-button h4" href="hero_laracroft.php">view hero</a>
+            <!-- Card 7 -->
+            <div class="portfolio-item graphic-design">
+                <div class="portfolio-bg" style="--img: url(../ressources/images/heroes/hero_laracroft.webp);"></div>
+                <div class="portfolio-description">
+                    <h4>Lara Croft</h4>
+                    <a class="link-button h4" href="hero_laracroft.php">show details</a>
+                </div>
             </div>
-        </div>
 
-        <!-- Card 8 -->
-        <div class="portfolio-item graphic-design">
+            <!-- Card 8 -->
+            <!-- <div class="portfolio-item graphic-design">
             <div class="portfolio-bg" style="--img: url(../ressources/images/heroes/hero_masterchief.webp);"></div>
             <div class="portfolio-description">
                 <h4>Masterchief</h4>
-                <!-- <p>Get rid of the bugs with Masterchief</p> -->
                 <a class="link-button h4" href="hero_masterchief.php">view hero</a>
             </div>
+        </div> -->
         </div>
     </div>
 
