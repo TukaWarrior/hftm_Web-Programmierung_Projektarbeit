d2f93a7a788c093affc72e4e395b2cc3f855a57f
Andres Soria <andres.soria@hftm.ch>
2023-10-14 21:19:48 +0200

    added about me pages

--- html/about.php
+++ html/about.php
@@ -18,30 +18,27 @@
     <!-- Top Navigation Bar -->
     <?php include 'element_topnav.php'; ?>
 
-    <!-- Content "who we are" -->
     <div class="blankContainer" style="--blankColor: transparent"></div>
+    
+    <!-- Content "who we are" -->
     <div class="blankPartition" style="--bgcolor: rgb(109, 180, 180)">
         <h2 class="mid">who we are</h2>
     </div>
 
     <div class="container">
         <div class="containerGrid">
-
             <div class="box box-1" style="--img: url(../ressources/images/avatar/andres.webp);"
                 data-text="Andres Soria">
-                <a class="link-button" href="404.php"></a>
+                <a class="link-button" href="about_andres.php"></a>
             </div>
-
             <div class="box box-2" style="--img: url(../ressources/images/avatar/luca.webp);"
                 data-text="Luca Bützberger">
-                <a class="link-button" href="404.php"></a>
+                <a class="link-button" href="about_luca.php"></a>
             </div>
-
             <div class="box box-3" style="--img: url(../ressources/images/avatar/nicola.webp);"
                 data-text="Nicola Jöhr">
-                <a class="link-button" href="404.php"></a>
+                <a class="link-button" href="about_nicola.php"></a>
             </div>
-
         </div>
     </div>
 
@@ -55,27 +52,22 @@
             "Rent-A-Hero" - Where your wildest dreams and most peculiar predicaments meet the world's quirkiest solution
             provider!
         </p>
-
         <p class="description">
             At "Rent-A-Hero," we believe in making everyday life an extraordinary adventure. Our superhero team is ready
             for any mission, no matter how mundane or eccentric. Just don't ask them to fix the Wi-Fi - even heroes have
             their limits!
         </p>
-
         <ul>
             <li>Tired of doing your laundry? "Rent-A-Hero" to the rescue! We send in caped crusaders to fold your socks
                 with unparalleled precision.</li></br>
-
             <li>Got a date that could use some spicing up? Our "Rent-A-Hero" dating package includes a sidekick who'll
                 narrate your evening with dramatic flair, turning mundane conversations into epic sagas of love.</li>
             </br>
-
             <li>Need someone to "accidentally" leave your family gathering? We've got the "Crisis-in-Cousinville"
                 special;
                 our heroes will stage a faux alien invasion, and your relatives will never forget the day they almost
                 met
                 extraterrestrials.</li></br>
-
             <li>Got a relentless pest problem? We've got "Insecta-Interceptors" who'll politely ask the bugs to leave,
                 or
                 maybe just throw a spectacular farewell party for them.</li></br>
---
+++ html/about_nicola.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="../css/styles.css" />
+    <link rel="stylesheet" href="../css/aboutMe.css" />
+    <link rel="stylesheet" href="../css/ComicStyle.css" />
+    <link rel="stylesheet" href="../css/footer.css" />
+    <title>About Andres</title>
+    <link rel="icon" type="image/x-icon" href="../ressources/images/favicon.webp" />
+
+    <style>
+        body {
+            background-image: url('../ressources/images/backgrounds/bg_about_nicola.webp');
+        }
+    </style>
+
+</head>
+
+<!---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- -->
+
+<body>
+
+    <!-- Top Navigation Bar -->
+    <?php include 'element_topnav.php'; ?>
+
+    <!-- Get Origin Story -->
+    <?php $aboutme = file_get_contents('../ressources/documents/about_nicola.html'); ?>
+
+    <div class="main">
+
+        <div class="blankContainer" style="--blankColor: transparent"></div>
+        <div class="blankPartition" style="--bgcolor: #94d6b0">
+            <h2 class="mid">Nicola Jöhr</h2>
+        </div>
+
+        <div class="row">
+            <div class="aboutMePanel" style="width: 350px;">
+                <p class="text top-left" style="font-size: 25px; font-weight: bold;">About me</p>
+                <p>
+                    <?php echo $aboutme; ?>
+                </p>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Bottom Footer Bar -->
+    <?php include 'element_footer.php'; ?>
+
+</body>
+
+</html>
